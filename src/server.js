398772bc07ed89476209eef301fc0bbe6225ffb7
nicolascaporaso <nicolascaporaso@gmail.com>
2023-06-19 20:40:27 -0300

    login

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 //@ts-check
+import MongoStore from 'connect-mongo';
 import express from 'express'
-import {apiRouter} from "./routes/api.routes.js";
+import session from 'express-session';
+import { apiRouter } from "./routes/api.routes.js";
 import { __dirname, connectMongo, connectSocket } from './utils.js';
 import path from "path";
 import handlebars from 'express-handlebars';
-import {viewRouter} from "./routes/view.Routes.js";
+import { viewRouter } from "./routes/view.Routes.js";
 import { ChatRouter } from './routes/chat.router.js';
+import { authRouter } from './routes/auth.router.js';
 
 const app = express();
 const port = 8081;
 
 const httpServer = app.listen(port, () => {
-    console.log("example app listen port " + "http://localhost:"+port+"/api/products");
+    console.log("example app listen port " + "http://localhost:" + port + "/api/products");
 });
 
 connectSocket(httpServer);
@@ -21,10 +24,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.engine ('handlebars', handlebars.engine());
-app.set('view engine','handlebars');
+app.use(
+    session({
+        store: MongoStore.create({ mongoUrl: "mongodb+srv://nicolascaporaso:control4970@comerce.nbjkdia.mongodb.net/ecomerce?retryWrites=true&w=majority", ttl: 7200 }),
+        secret: 'un-re-secreto',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 
 app.use("/", viewRouter);
 app.use("/api", apiRouter);
 app.use("/chat", ChatRouter);
+app.use('/auth', authRouter);
